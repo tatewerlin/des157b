bb1fa6a9ca97c513f49e6cf0455033c2ce9aafbf
tatewerlin <tatebwerlin@gmail.com>
2025-04-07 20:25:42 -0700

    first draft of portal page

--- script.js
+++ script.js
@@ -1,25 +1,67 @@
 (()=>{
     'use strict';
     console.log('reading js');
-    const uls = document.querySelectorAll('#clouds ul');
-    
-    styleLinks(); // sets the background color for each link cloud independently
+    const mySwitch = document.querySelector("#switch");
+    const body = document.querySelector('body');
+    const html = document.querySelector('html');
 
-    function styleLinks(){
-        for(let i=0; i<uls.length; i++){
-            let thisLink = uls[i].querySelectorAll('a');
-            let colorString = `var(--link-bg-${i+1})`;
-            for(let j=0; j<thisLink.length; j++){
-                thisLink[j].style.backgroundColor = colorString;
-                thisLink[j].style.borderBottomColor = colorString;
-                thisLink[j].addEventListener('mouseover', () => {
-                    thisLink[j].style.borderBottomColor = 'var(--default-dark)';
-                    console.log('hovering')
-                });
-                thisLink[j].addEventListener('mouseout', () => {
-                    thisLink[j].style.borderBottomColor = colorString;
-                });
-            }
+    const links = document.querySelectorAll("#clouds ul a");
+    const arrows = document.querySelectorAll('.link-arrow');
+    const switchSpans = document.querySelectorAll('#switch span');
+
+    let switchCounter = 0; // stores the number of times the switch has been clicked
+
+    changeColors(switchCounter); // ensures everything is the right color
+    hideArrows();
+    manageLinkHover();
+    manageSwitch();
+
+    function hideArrows(){ // hide the arrows on page load
+        for (let i=0; i<arrows.length; i++){
+            arrows[i].classList.add('hidden');
         }
     }
+
+    function manageLinkHover(){
+        for (let i=0; i<links.length; i++){
+            links[i].addEventListener('mouseover', () => {
+                arrows[i].classList.toggle('hidden');
+                if (switchCounter%2!==0){ // darkmode colors here
+                    links[i].style.color = 'var(--default-light)';
+                }
+                else{ // lightmode colors here
+                    links[i].style.color = 'var(--default-dark)';
+                }
+            });
+            links[i].addEventListener('mouseout', () => {
+                arrows[i].classList.toggle('hidden');
+                links[i].style.color = 'var(--offdark)';
+            })
+        }
+    }
+
+    function manageSwitch(){ // manage behavior when switch is clicked
+        mySwitch.addEventListener('click', () => {
+            switchCounter++;
+            console.log('switch flipped');
+            changeColors(switchCounter);
+        });
+    }
+
+    function changeColors(s){ // updates all colors
+        if (s%2!==0){ // darkmode colors here
+            body.style.backgroundColor = 'var(--default-dark)';
+            html.style.color = 'var(--default-light)';
+            switchSpans[0].style.color = 'var(--offdark)';
+            switchSpans[1].style.color = 'var(--default-light)';
+        }
+        else{ // lightmode colors here
+            body.style.backgroundColor = 'var(--default-light)';
+            html.style.color = 'var(--default-dark)';
+            switchSpans[0].style.color = 'var(--default-dark)';
+            switchSpans[1].style.color = 'var(--offlight)';
+        }
+    }
+
+
 })();
