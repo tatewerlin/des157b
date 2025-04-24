6181eaa61564d42cfe007e08cc809c09b42f22ea
tatewerlin <tatebwerlin@gmail.com>
2025-04-24 14:24:14 -0700

    add figjam board link

--- ministudio/studio2/script.js
+++ ministudio/studio2/script.js
@@ -1,12 +1,17 @@
 (()=>{
     'use strict';
     console.log('reading js');
-
     const back = document.querySelector('#back-arrow')
-    back.addEventListener('mouseover', () => {
-        back.classList.add('itemHover');
-    });
-    back.addEventListener('mouseout', () => {
-        back.classList.remove('itemHover');
-    });
+
+    function manageItemHover(){
+        back.addEventListener('mouseover', () => {
+            back.classList.add('itemHover');
+        });
+        back.addEventListener('mouseout', () => {
+            back.classList.remove('itemHover');
+        });
+    }
+
+    manageItemHover();
+    
 })();
